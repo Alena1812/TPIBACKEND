3b6d1bd1708145de0241c07051c64505df0dfce6
Justo Valentin Oberto <96108@sistemas.utn.frc.edu.ar>
2024-11-06 17:41:11 -0300

    cambie el Restriccionesservice y ahora funciona

--- agencia/src/main/java/com/tpi/agencia/services/RestriccionesService.java
+++ agencia/src/main/java/com/tpi/agencia/services/RestriccionesService.java
@@ -1,10 +1,14 @@
 package com.tpi.agencia.services;
 
 import com.tpi.agencia.dtos.RestriccionesDto;
-import jakarta.persistence.Cacheable;
-import lombok.Value;
+import com.tpi.agencia.dtos.expertos.NotificacionRadioExcedidoDto;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class RestriccionesService {
@@ -14,11 +18,15 @@ public class RestriccionesService {
     @Value("${tpi-agencia.microservicio-restricciones.url}")
     private String urlRestricciones;
 
-    @Value(("${tpi-agencia.microservicio-notificaciones.url}"))
+    @Value("${tpi-agencia.microservicio-notificaciones.url}")
     private String urlNotificaciones;
 
     @Cacheable("restrictionsApiCache")
     public RestriccionesDto getRestricciones() {
         return restTemplate.getForObject(urlRestricciones, RestriccionesDto.class);
+    }
+    public List<NotificacionRadioExcedidoDto> getNotificacionesRadioExcedido() {
+        NotificacionRadioExcedidoDto[] notificacionesArray = restTemplate.getForObject(urlNotificaciones + "/notificaciones/seguridad/radio-excedido", NotificacionRadioExcedidoDto[].class);
+        return Arrays.asList(notificacionesArray);
     }
 }
